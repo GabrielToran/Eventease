772e8f16e54de34a74129d487b73f9d86a83e832
GabrielToran <toranigabi@gmail.com>
2025-10-21 19:57:44 -0400

    Add files via upload

---
+++ eventease/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+ const eventRoutes = require('./routes/events');
+const registrationRoutes = require('./routes/registrations');
+const userRoutes = require('./routes/users');
+//const feedbackRoutes = require('./routes/feedback');
+const adminRoutes = require('./routes/admin');
+const categoryRoutes = require('./routes/categories');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require('./config/db');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/registrations', registrationRoutes);
+app.use('/api/users', userRoutes);
+//app.use('/api/feedback', feedbackRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/categories', categoryRoutes);
+
+// Test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Backend is working!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
