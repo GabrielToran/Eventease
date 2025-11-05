33fbe4bb97af002735b0bf57fd7e99570c256c62
GabrielToran <toranigabi@gmail.com>
2025-11-05 14:39:33 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,20 @@
+const { Pool } = require('pg');
+require('dotenv').config();
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('✅ Database connected successfully');
+  release();
+});
+
+module.exports = pool;
